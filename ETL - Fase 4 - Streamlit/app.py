7d441503ba69487f227dfab1bba351419c679b86
DiogoBernardoPT <diogobernardo888@hotmail.com>
2024-10-28 18:26:57 +0100

    First try on streamlit run - Works

---
+++ ETL - Fase 4 - Streamlit/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard de Airbnb", page_icon="🏠", layout="wide")
+
+st.title("Dashboard de Análisis de Datos de Airbnb")
+st.markdown("""
+    Bienvenido al dashboard de análisis de datos de Airbnb! Aquí puedes explorar la información
+    sobre las propiedades, precios y ubicaciones disponibles. Usa los filtros para personalizar tu 
+    visualización.
+""")
+
