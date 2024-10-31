192f4f2a5b6ee5a69b13b60101e1725828d90d68
Sandra <mirambellsandra@gmail.com>
2024-10-31 18:04:51 +0100

    Modificado

--- ETL - Fase 4 - Streamlit/app.py
+++ ETL - Fase 4 - Streamlit/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
 st.set_page_config(page_title="Dashboard de Airbnb", page_icon="🏠", layout="wide")
 
 st.title("Dashboard de Análisis de Datos de Airbnb")
@@ -8,4 +9,12 @@ st.markdown("""
     sobre las propiedades, precios y ubicaciones disponibles. Usa los filtros para personalizar tu 
     visualización.
 """)
-
+# Control deslizante para seleccionar rango
+rango = st.slider("Selecciona un rango", 0, 100, (25, 75))
+# Cargar los datos
+datos = pd.read_csv('C:/Users/peni_/Desktop/proyecto/Proyecto-Final-Bootcamp---Dream-Team/df_clean.csv')
+# Filtrar los datos por el rango seleccionado
+datos_filtrados = datos[(datos["Prices_per_night"] > rango[0]) & (datos["Prices_per_night"] < rango[1])]
+st.write(datos.head())  # Muestra las primeras filas para confirmar la carga
+# Visualización de gráfico seleccionando solo la columna de precios
+st.line_chart(datos_filtrados["Prices_per_night"])
