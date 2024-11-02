be8050a7f3f3709b64327bfaae90eb40436e39a2
DiogoBernardoPT <diogobernardo888@hotmail.com>
2024-11-02 19:55:06 +0100

    Updated app.py with landing page + graphs, created modules and images ffolders for organization, and solved CSV path to automatize use

--- ETL - Fase 4 - Streamlit/app.py
+++ ETL - Fase 4 - Streamlit/app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
+from modules import visualizations
+
 st.set_page_config(page_title="Dashboard de Airbnb", page_icon="🏠", layout="wide")
 
 st.title("Dashboard de Análisis de Datos de Airbnb")
@@ -9,12 +12,74 @@ st.markdown("""
     sobre las propiedades, precios y ubicaciones disponibles. Usa los filtros para personalizar tu 
     visualización.
 """)
-# Control deslizante para seleccionar rango
-rango = st.slider("Selecciona un rango", 0, 100, (25, 75))
+
 # Cargar los datos
-datos = pd.read_csv('C:/Users/peni_/Desktop/proyecto/Proyecto-Final-Bootcamp---Dream-Team/df_clean.csv')
-# Filtrar los datos por el rango seleccionado
-datos_filtrados = datos[(datos["Prices_per_night"] > rango[0]) & (datos["Prices_per_night"] < rango[1])]
-st.write(datos.head())  # Muestra las primeras filas para confirmar la carga
-# Visualización de gráfico seleccionando solo la columna de precios
-st.line_chart(datos_filtrados["Prices_per_night"])
+uploader = st.file_uploader('Sube el archivo CSV de datos', type='csv')
+if uploader:
+    df = pd.read_csv(uploader)
+    
+
+# Barra lateral para navegar entre las pestañas
+page = st.sidebar.selectbox("Navega por las secciones", ["Home", "Data Analysis", "Price Analysis", "Locations", "About"])
+
+# Sección "Home"
+if page == "Home":
+    st.title("Análisis de Datos de AirBnB")
+    st.write("""
+    **Bienvenido al dashboard de análisis de datos de Airbnb!**  
+    Explora información sobre precios, valoraciones y ubicaciones de los alojamientos.
+    """)
+    st.image("images/airbnb_1stpage.png", width=700)  # Ruta de la imagen en la carpeta 'images'
+
+# Sección "Data Analysis"
+elif page == "Data Analysis":
+    st.header("Análisis Exploratorio de Datos")
+    st.write("Gráficos y estadísticas de las propiedades de Airbnb.")
+
+    st.subheader("Primeras Filas del DataFrame")
+    st.dataframe(df.head())
+
+
+    # LLamar la funciones en visualizations.py
+    visualizations.rating_distribution(df) # Distribución de Ratings
+    visualizations.property_type_distribution(df) # Distribución de Tipos de Propiedades
+    visualizations.beds_ratings_scatter(df) # Relación entre Número de Camas y Ratings
+    visualizations.beds_distribution_histogram(df) # Distribuição do Número de Camas
+    visualizations.reviews_rating_distribution(df) # Distribuição de Reseñas y Valoraciones por tipo de alojamiento  
+
+
+    # Agregar otros gráficos relevantes para análisis de datos (Estudiar graficos interesantes hechos en el EDA)
+
+# Sección "Price Analysis"
+elif page == "Price Analysis":
+    st.header("Análisis de Precios")
+    st.write("""
+    En esta sección, exploraremos cómo los precios de las propiedades de Airbnb varían según el tipo de alojamiento y la ubicación. 
+    Analizaremos las tendencias de precios, las distribuciones de ratings y la relación entre precios y número de reseñas. 
+    Esto nos ayudará a comprender mejor el mercado de Airbnb y a identificar posibles patrones y oportunidades.
+""")
+    
+# Agregar el control deslizante para seleccionar el rango de precios
+    rango = st.slider("Selecciona un rango de precios", 
+                    min_value=int(df["Prices_per_night"].min()), 
+                    max_value=int(df["Prices_per_night"].max()), 
+                    value=(25, 75))
+    
+    # Filtrar los datos por el rango de precios seleccionado
+    datos_filtrados = df[(df["Prices_per_night"] > rango[0]) & (df["Prices_per_night"] < rango[1])]
+    
+    
+    visualizations.price_property_types(df)  # Llama a la función para el gráfico por tipo de propiedad
+    visualizations.price_rating_distribution(df)  # Llama a la función para la distribución de precio/rating
+    visualizations.price_distribution_histogram(df)  # Distribución de Precios por Noche
+    visualizations.average_price_by_capacity(df) # Precio Medio por Capacidad Máxima de Huéspedes
+    visualizations.price_distribution_bar_chart(df)  # Gráfico de barras da distribuição de preços
+
+
+# Sección "About"
+elif page == "About":
+    st.header("Sobre el Proyecto")
+    st.write("""
+    Este dashboard fue creado como parte de un proyecto colaborativo de análisis de datos de Airbnb.
+    """)
+
---
+++ ETL - Fase 4 - Streamlit/modules/visualizations.py
@@ -0,0 +1,109 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Función para el gráfico de distribución de valoraciones
+def rating_distribution(df):
+    st.subheader("Distribución de Ratings")
+    rating_hist = px.histogram(df, x="Ratings", nbins=20, title="Distribución de Ratings")
+    st.plotly_chart(rating_hist, use_container_width=True, key="rating_hist")
+
+
+# Función para el gráfico de precio por tipo de propiedad
+def price_property_types(df):
+    # st.header("Precio por Tipo de Propiedad")
+    price_chart = px.box(df, x="Property_types", y="Prices_per_night", title="Precios por Tipo de Alojamiento")
+    st.plotly_chart(price_chart, use_container_width=True, key="price_property_types")
+
+# Función para la gráfica de visualización entre Precio/Ratings por tipo de alojamiento
+def price_rating_distribution(df):
+    price_rating_distr_log = px.scatter(
+        data_frame=df,
+        x="Ratings",
+        y="Prices_per_night",
+        log_x=True,
+        color="Property_types",
+        title="Distribución de Precio y Valoración por Tipo de Alojamiento (Escala Logarítmica)",
+        labels={"Ratings": "Valoración (escala logarítmica)", "Prices_per_night": "Precio por Noche"},
+    )
+    st.plotly_chart(price_rating_distr_log, use_container_width=True)
+
+# Función para la gráfica de precio medio por capacidad máxima de huéspedes
+def average_price_by_capacity(df):
+    # Gráfico de barras que ilustra el precio medio según la capacidad máxima de huéspedes.
+    avg_price = df.groupby('Maximum_guests')['Prices_per_night'].mean().reset_index()
+    fig = px.bar(avg_price, x='Maximum_guests', y='Prices_per_night', 
+                title="Precio Medio por Capacidad Máxima de Huéspedes")
+    st.plotly_chart(fig, use_container_width=True)    
+
+def price_distribution_histogram(df):
+    fig = px.histogram(df, x='Prices_per_night', nbins=30, 
+                    title="Distribución de Precios por Noche")
+    st.plotly_chart(fig, use_container_width=True)
+
+def price_distribution_bar_chart(df):
+    # Agrupar por intervalos de preço
+    intervalos = pd.cut(df["Prices_per_night"], bins=10)
+    conteo_por_intervalo = df.groupby(intervalos).size()
+
+    # Criar gráfico de barras
+    bar_chart = px.bar(x=conteo_por_intervalo.index.astype(str), 
+                        y=conteo_por_intervalo.values, 
+                        labels={'x': 'Intervalos de Preço', 'y': 'Contagem'},
+                        title='Distribución de Precios en el Rango Seleccionado')
+    
+    # Mostrar o gráfico
+    st.plotly_chart(bar_chart, use_container_width=True)
+
+# Función para la gráfica de distribución de Reseñas y Valoraciones por tipo de alojamiento
+def reviews_rating_distribution(df):
+    reviews_rating_distr = px.scatter(
+        data_frame=df,
+        x="Ratings",
+        y="Num_reviews",
+        log_x=True,
+        color="Prices_per_night",
+        title="Distribución de Reseñas y Valoración por Tipo de Alojamiento (Escala Logarítmica)",
+        hover_name="Property_types",
+        opacity=0.5,
+        size="Prices_per_night",
+        size_max=15,
+    )
+    st.plotly_chart(reviews_rating_distr, use_container_width=True)
+
+# Función para la gráfica de distribución de tipos de propiedades
+def property_type_distribution(df):
+    property_count = df['Property_types'].value_counts()
+    fig = px.pie(property_count, values=property_count.values, names=property_count.index, 
+                title="Distribución de Tipos de Propiedades")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Función para la gráfica de histograma de número de camas
+def beds_distribution_histogram(df):
+    fig = px.histogram(df, x='Camas', nbins=30, 
+                    title="Distribución del Número de Camas")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Función para la gráfica de relación entre número de camas y ratings
+def beds_ratings_scatter(df):
+    # Gráfico de dispersión que muestra la relación entre el número de camas y las valoraciones.
+    fig = px.scatter(df, x='Camas', y='Ratings', color='Property_types',
+                    title="Relación entre Número de Camas y Ratings",
+                    labels={"Camas": "Número de Camas", "Ratings": "Ratings"})
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
